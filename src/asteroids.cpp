3070084b16a275bbd41168d56aafd205f8d62556
Bubu-Droid <fufudadw@gmail.com>
2026-06-17 23:52:43 +0530

    Format code

--- src/asteroids.cpp
+++ src/asteroids.cpp
@@ -1,17 +1,29 @@
 #include "asteroids.h"
-#include "constants.h"
-#include "raymath.h"
-#include <array>
+
 #include <memory>
 
-Asteroid CreateAsteroid(Vector2 position, Vector2 velocity, AsteroidSize size,
-                        float rotationAngle, float rotationSpeed) {
+#include "constants.h"
+#include "raymath.h"
+
+Asteroid CreateAsteroid(
+    Vector2 position,
+    Vector2 velocity,
+    AsteroidSize size,
+    float rotationAngle,
+    float rotationSpeed
+) {
   std::unique_ptr<Asteroid> Ast = std::make_unique<Asteroid>(
-      true, position, velocity, size, rotationAngle, rotationSpeed);
+      true,
+      position,
+      velocity,
+      size,
+      rotationAngle,
+      rotationSpeed
+  );
   return *Ast;
 }
 
-void UpdateAsteroid(Asteroid &asteroid, float &frametime, float &time) {
+void UpdateAsteroid(Asteroid& asteroid, float& frametime, float& time) {
   if (time > asteroid.creationTime + AST_LIFETIME) {
     asteroid.active = false;
     return;
@@ -26,29 +38,40 @@ void UpdateAsteroid(Asteroid &asteroid, float &frametime, float &time) {
   asteroid.rotationAngle += asteroid.rotationSpeed * frametime;
 }
 
-void DrawAsteroid(Asteroid &asteroid) {
+void DrawAsteroid(Asteroid& asteroid) {
   if (asteroid.active) {
-    DrawPolyLines(asteroid.position, 3, 16 * asteroid.asteroidSize,
-                  asteroid.rotationAngle, WHITE);
+    DrawPolyLines(
+        asteroid.position,
+        6,
+        16 * asteroid.asteroidSize,
+        asteroid.rotationAngle,
+        WHITE
+    );
   }
 }
 
 Vector2 GetNextAsteroidPosition(int astDir) {
-  float screenRadius = std::sqrt(std::pow((SCREEN_HEIGHT / 2.0f), 2) +
-                                 std::pow((SCREEN_WIDTH / 2.0f), 2));
+  float screenRadius = std::sqrt(
+      std::pow((SCREEN_HEIGHT / 2.0f), 2) + std::pow((SCREEN_WIDTH / 2.0f), 2)
+  );
   Vector2 newAstPos = Vector2Rotate(Vector2(1, 0), astDir);
   newAstPos = Vector2Scale(newAstPos, (screenRadius + AST_SPAWN_PADDING));
   newAstPos = Vector2Add(newAstPos, SCREEN_CENTER);
   return newAstPos;
 }
 
-void AddAsteroid(std::array<Asteroid, MAX_ASTEROIDS> &asteroids,
-                 Vector2 position, AsteroidSize size, int randVelocity,
-                 float randAngleNoise, int randDirection,
-                 float randRotationSpeed, std::array<Vector2, 2> &line0,
-                 std::array<Vector2, 2> &line1) {
-
-  for (auto &asteroid : asteroids) {
+void AddAsteroid(
+    std::array<Asteroid, MAX_ASTEROIDS>& asteroids,
+    Vector2 position,
+    AsteroidSize size,
+    int randVelocity,
+    float randAngleNoise,
+    int randDirection,
+    float randRotationSpeed,
+    std::array<Vector2, 2>& line0,
+    std::array<Vector2, 2>& line1
+) {
+  for (auto& asteroid : asteroids) {
     if (asteroid.active) {
       continue;
     }
@@ -61,21 +84,34 @@ void AddAsteroid(std::array<Asteroid, MAX_ASTEROIDS> &asteroids,
       line0[0] = position;
       line1[0] = position;
 
-      line0[1] =
-          Vector2Add(position, Vector2Rotate(Vector2Scale(normVelocity, 1000),
-                                             -ASTEROID_RANDOM_ANGLE));
-      line1[1] =
-          Vector2Add(position, Vector2Rotate(Vector2Scale(normVelocity, 1000),
-                                             ASTEROID_RANDOM_ANGLE));
+      line0[1] = Vector2Add(
+          position,
+          Vector2Rotate(
+              Vector2Scale(normVelocity, 1000),
+              -ASTEROID_RANDOM_ANGLE
+          )
+      );
+      line1[1] = Vector2Add(
+          position,
+          Vector2Rotate(Vector2Scale(normVelocity, 1000), ASTEROID_RANDOM_ANGLE)
+      );
     }
 
     velocity = Vector2Rotate(velocity, randAngleNoise);
 
-    asteroid = CreateAsteroid(position, velocity, size, randDirection,
-                              randRotationSpeed);
+    asteroid = CreateAsteroid(
+        position,
+        velocity,
+        size,
+        randDirection,
+        randRotationSpeed
+    );
     return;
   }
 
-  TraceLog(LOG_ERROR, "Failed to create an asteroid because there was no "
-                      "inactive spot in the array!");
+  TraceLog(
+      LOG_ERROR,
+      "Failed to create an asteroid because there was no "
+      "inactive spot in the array!"
+  );
 }
--- src/asteroids.h
+++ src/asteroids.h
@@ -36,20 +36,41 @@ struct Asteroid {
 
   Asteroid() : active(false) {}
 
-  Asteroid(bool active, Vector2 position, Vector2 velocity,
-           AsteroidSize asteroidSize, float rotationAngle, float rotationSpeed)
-      : active(active), position(position), velocity(velocity),
-        asteroidSize(asteroidSize), rotationAngle(rotationAngle),
-        rotationSpeed(rotationSpeed), creationTime(GetTime()) {}
+  Asteroid(
+      bool active,
+      Vector2 position,
+      Vector2 velocity,
+      AsteroidSize asteroidSize,
+      float rotationAngle,
+      float rotationSpeed
+  ) :
+      active(active),
+      position(position),
+      velocity(velocity),
+      asteroidSize(asteroidSize),
+      rotationAngle(rotationAngle),
+      rotationSpeed(rotationSpeed),
+      creationTime(GetTime()) {}
 };
 
-Asteroid CreateAsteroid(Vector2 position, Vector2 velocity, AsteroidSize size,
-                        float rotationAngle, float rotationSpeed);
-void UpdateAsteroid(Asteroid &asteroid, float &frametime, float &time);
-void DrawAsteroid(Asteroid &asteroid);
+Asteroid CreateAsteroid(
+    Vector2 position,
+    Vector2 velocity,
+    AsteroidSize size,
+    float rotationAngle,
+    float rotationSpeed
+);
+void UpdateAsteroid(Asteroid& asteroid, float& frametime, float& time);
+void DrawAsteroid(Asteroid& asteroid);
 Vector2 GetNextAsteroidPosition(int astDir);
-void AddAsteroid(std::array<Asteroid, MAX_ASTEROIDS> &asteroids,
-                 Vector2 position, AsteroidSize size, int randVelocity,
-                 float randAngleNoise, int randDirection,
-                 float randRotationSpeed, std::array<Vector2, 2> &line0,
-                 std::array<Vector2, 2> &line1);
+void AddAsteroid(
+    std::array<Asteroid, MAX_ASTEROIDS>& asteroids,
+    Vector2 position,
+    AsteroidSize size,
+    int randVelocity,
+    float randAngleNoise,
+    int randDirection,
+    float randRotationSpeed,
+    std::array<Vector2, 2>& line0,
+    std::array<Vector2, 2>& line1
+);
