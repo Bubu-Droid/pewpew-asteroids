0cfb0cd327d5a8913bf801125216418702c83018
Bubu-Droid <fufudadw@gmail.com>
2026-06-17 23:53:37 +0530

    Format code

--- src/constants.h
+++ src/constants.h
@@ -2,12 +2,14 @@
 
 #include "raylib.h"
 
-#define NEARBLACK CLITERAL(Color){15, 15, 15, 255}
+#define NEARBLACK CLITERAL(Color) {15, 15, 15, 255}
 
 static const int SCREEN_WIDTH = 600;
 static const int SCREEN_HEIGHT = 600;
-static const Vector2 SCREEN_CENTER = {SCREEN_WIDTH / 2.0f,
-                                      SCREEN_HEIGHT / 2.0f};
+static const Vector2 SCREEN_CENTER = {
+    SCREEN_WIDTH / 2.0f,
+    SCREEN_HEIGHT / 2.0f
+};
 
 static bool debug = false;
 
